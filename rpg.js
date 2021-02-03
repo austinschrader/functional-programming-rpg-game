a345d1b5a85e34fadd50a02d0b5ad6f974c962bb
Austin Schrader <austin.jschrader@gmail.com>
2021-02-03 10:30:26 -0800

    Add state management functions

---
+++ rpg.js
@@ -0,0 +1,21 @@
+const storeState = () => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState
+  }
+}
+
+const stateControl = storeState();
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value
+    })
+  }
+}
+
+const 
