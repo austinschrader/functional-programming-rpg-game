98bf45f7f3bae652c9c5f5187f17f2c951513f23
Austin Schrader <austin.jschrader@gmail.com>
2021-02-03 14:33:16 -0800

    Add functionality to dynamically create balrog
    co-authored-by: cory nordenbrock <cordenbrock@gmail.com>

--- rpg.js
+++ rpg.js
@@ -53,26 +53,30 @@ const activateGandalfIntel = changeState("intelligence", "magic")(105, 54); // S
 const newCharacterState2 = updateStateObj(activateGandalfIntel, gandalf); // Push it to state
 
 // Grab current state
-const currentState = updateStateObj();
-console.log(`this is the intelligence level way #1: ${currentState.Gandalf.intelligence}`);
-console.log(`this is the intelligence level way #2: ${newCharacterState2.Gandalf.intelligence}`);
+const currentState1 = updateStateObj();
+// console.log(`this is the intelligence level way #1: ${currentState.Gandalf.intelligence}`);
+// console.log(`this is the intelligence level way #2: ${newCharacterState2.Gandalf.intelligence}`);
 
 // Update Wizard with Multiple Props - Target nested storeObj value
-const deathOfGandalf = changeState("intelligence", "magic")(-(updateStateObj().Gandalf.intelligence), -(updateStateObj().Gandalf.magic));
-const newCharacterState3 = updateStateObj(deathOfGandalf, gandalf);
+// const deathOfGandalf = changeState("intelligence", "magic")(-(updateStateObj().Gandalf.intelligence), -(updateStateObj().Gandalf.magic));
+// const newCharacterState3 = updateStateObj(deathOfGandalf, gandalf);
 
-console.log("state obj", newCharacterState);
-console.log("state obj", newCharacterState2);
-console.log("state obj", newCharacterState3);
+// console.log("state obj", newCharacterState);
+// console.log("state obj", newCharacterState2);
+// console.log("state obj", newCharacterState3);
 
 // Battle starts
 // if newcharacter state 2 is less than or equal to current character state; && magic/intelligence depleted
 
-function battle(fighter1) {
-  const balrog = "Balrog";
-  const newBalrog = addCharacter(defaultBalrogObj)(balrog);
-  const newBalrogState = updateStateObj(newBalrog);
-  console.log(newBalrogState);
+function battle(currentState) {
+  if (!currentState1.Balrog) {
+    balrog = "Balrog";
+    newBalrog = addCharacter(defaultBalrogObj)(balrog);
+    newBalrogState = updateStateObj(newBalrog);
+  }
+
+  const currentStateBattleStart = updateStateObj();
+  console.log(currentStateBattleStart);
   // termination case
 
 
@@ -80,17 +84,14 @@ function battle(fighter1) {
   // if either balrog or galdalf's currentstate.Gandalf.intelligence <= 0 then the fight is over
 
   // recursion
-
-
   // fighter1 attacks first, and so fighter2 gets their stats reduced first
   // the reduction of fighter2's stats depends on fighter1's strength/magic
   const firstAttack = changeState("intelligence", "magic")(-10, -10);
-  
-  console.log(currentState.Gandalf, currentState.Balrog);
+
   // return battle();
   return;
 }
 
-battle(currentState.Gandalf);
+battle(currentState1);
 
 
